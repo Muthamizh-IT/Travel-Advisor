daa0fa190329774698949ba52f9967e833371b2e
muthamizhyadav <muthamizhyadav@gmail.com>
2023-08-31 11:45:48 +0530

    ghdgfd

--- src/models/Drawing.model.js
+++ src/models/Drawing.model.js
@@ -1,32 +1,39 @@
 const mongoose = require('mongoose');
 const { v4 } = require('uuid');
-const DrawingAppSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    default: v4,
+const DrawingAppSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: v4,
+    },
+    id: {
+      type: Number,
+    },
+    outLine_img_Url: {
+      type: String,
+    },
+    original_img_Url: {
+      type: String,
+    },
+    time: {
+      type: String,
+    },
+    originalColor: {
+      type: Array,
+    },
+    applyColor: {
+      type: Array,
+    },
+    level: {
+      type: Number,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
   },
-  id: {
-    type: Number,
-  },
-  outLine_img_Url: {
-    type: String,
-  },
-  original_img_Url: {
-    type: String,
-  },
-  time: {
-    type: String,
-  },
-  originalColor: {
-    type: Array,
-  },
-  applyColor: {
-    type: Array,
-  },
-  level: {
-    type: Number,
-  },
-});
+  { timestamps: true }
+);
 
 const Drawing = mongoose.model('drawing', DrawingAppSchema);
 
